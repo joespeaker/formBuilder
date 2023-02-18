e7526410e94f98b3d193c380ac4518675bf34449
joespeaker <joebieker2486@outlook.com>
2023-02-17 19:37:32 -0600

    update

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function dragStart(ev) {
 function dragDrop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    nodeCopy.id = "id-" + getRandomInt(1, 10000); /* We cannot use the same ID */
+    ev.target.appendChild(nodeCopy);
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function drop(ev)
+{
+ev.preventDefault();
+var data=ev.dataTransfer.getData("Text");
+var el = document.getElementById(data);
+el.parentNode.removeChild(el);
 }
